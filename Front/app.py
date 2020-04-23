2bb5d7d9bcf228a9d02cb79d1aa2aca1d2ad0962
liamhebert <liamhebert@gmail.com>
2020-04-23 20:35:42 -0300

    FRONT-005: Slider!

--- Front/app.py
+++ Front/app.py
@@ -2,8 +2,7 @@ import dash
 import dash_daq as daq
 import dash_core_components as dcc
 import dash_html_components as html
-
-
+from dash.dependencies import Input, Output
 app = dash.Dash()
 
 app.layout = html.Div(
@@ -28,32 +27,40 @@ app.layout = html.Div(
         html.Div(
             id="Graduated Bars",
             children=[
-                daq.GraduatedBar(
+                daq.Slider(
                     id='O2 Concentration Slider',
                     min=20,
+                    size=400,
                     max=100,
                     step=5,
+                    handleLabel={"showCurrentValue": True, "label": "O2%"},
                     value=30,
                 ),
-                daq.GraduatedBar(
+                daq.Slider(
                     id='PEEP Slider',
                     min=0,
+                    size=400,
                     max=50,
                     step=1,
+                    handleLabel={"showCurrentValue": True, "label": "cmH2O"},
                     value=20,
                 ),
-                daq.GraduatedBar(
+                daq.Slider(
                     id='Resp Rate Slider',
                     min=6,
+                    size=400,
                     max=40,
                     step=1,
+                    handleLabel={"showCurrentValue": True, "label": "b/min"},
                     value=20,
                 ),
-                daq.GraduatedBar(
+                daq.Slider(
                     id='Tidal Volume Slider',
                     min=500,
+                    size=400,
                     max=2000,
                     step=20,
+                    handleLabel={"showCurrentValue": True, "label": "ml"},
                     value=1000,
                 )
             ]
