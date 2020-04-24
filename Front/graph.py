5b9547c5596dbb1599af533fc6f2649f69fcf77d
lmarchand <lr374843@dal.ca>
2020-04-23 21:46:45 -0300

    updated pressure, oxygen, volume graphs and added some styling

--- Front/graph.py
+++ Front/graph.py
@@ -6,18 +6,44 @@ import plotly.graph_objects as go
 
 df = pd.read_csv('testgraphvalues.csv')
 
-fig = go.Figure(go.Scatter(x=df['Time'], y=df['Value'], name='cmH2O'))
+pressure = go.Figure(go.Scatter(x=df['Time'], y=df['Value'], name='cmH2O',
+                                marker=dict(color='#F0CF45')))
+pressure.update_layout(title='Pressure',
+                       plot_bgcolor='#000000',
+                       showlegend=True)
 
-fig.update_layout(title='Pressure',
-                  plot_bgcolor='rgb(230, 230,230)',
-                  showlegend=True)
+oxygen = go.Figure(go.Scatter(x=df['Time'], y=df['Value'], name='l/min',
+                              marker=dict(color='#15A71B')))
+oxygen.update_layout(title='Oxygen',
+                     plot_bgcolor='#000000',
+                     showlegend=True)
 
-fig.show()
+volume = go.Figure(go.Scatter(x=df['Time'], y=df['Value'], name='mL',
+                              marker=dict(color='#6A96DD')))
+volume.update_layout(title='Volume',
+                     plot_bgcolor='#000000',
+                     showlegend=True)
 
 app = dash.Dash(__name__)
 app.layout = html.Div([
-    dcc.Graph(figure=fig)
+    html.Div([
+        dcc.Graph(
+            id='pressure',
+            figure=pressure
+        ),
+    ]),
+    html.Div([
+        dcc.Graph(
+            id='oxygen',
+            figure=oxygen),
+    ]),
+    html.Div([
+        dcc.Graph(
+            id='volume',
+            figure=volume),
+    ]),
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
